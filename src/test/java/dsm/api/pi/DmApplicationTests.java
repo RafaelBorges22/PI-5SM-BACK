4945bb3416395b644bf14802168a2c45cae3d6e7
RafaelBorges22 <rafaelmascarenhasborges@gmail.com>
2026-02-24 19:09:47 -0300

    Preparação de ambiente backend :fire:

---
+++ src/test/java/dsm/api/pi/DmApplicationTests.java
@@ -0,0 +1,13 @@
+package dsm.api.pi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
